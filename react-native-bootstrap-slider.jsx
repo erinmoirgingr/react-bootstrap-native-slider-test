9823b7b0fb62c74a7e0ea198f3f9a1b62d75706f
blackjk3 <blackjk@gmail.com>
2015-08-18 10:05:33 -0500

    Need the bootstrap slider only.

--- react-native-bootstrap-slider.jsx
+++ react-native-bootstrap-slider.jsx
@@ -3,32 +3,14 @@ var SliderNative = function(
     $,
     BootstrapSlider,
     BrowserDetectMixin) {
-    return React.createClass({
-        // Front end to the HTML5 native slider, i.e <input type="range">
-        render: function () {
-            return (
-                <input id="mySlider"
-                    type="range"
-                    value={this.props.value}
-                    min={this.props.min}
-                    max={this.props.max}
-                    onInput={this.props.handleChange}
-                    onChange={this.handleOnChange}
-                    step={this.props.step} />
-            );
-        },
-        handleOnChange: function () {
-            // Nothing to do here.  Only present to prevent reactjs warning
-            // about onChange not being present
-        }
-    });
-
-    var BootstrapSlider = React.createClass({
+    
+    return React.createClass({displayName: "BootstrapSlider",
         // Bootstrap-slider.js from https://github.com/seiyria/bootstrap-slider
         render: function () {
+          console.log(this.props);
             // The slider's an input.  That's all we need.  We'll do the rest in JS.
             return (
-                <input />
+                React.createElement("input", null)
             );
         },
         componentDidMount: function () {
@@ -43,18 +25,19 @@ var SliderNative = function(
                     target: {}
                 };
                 fakeEvent.target.value = e.value.newValue;
-                that.props.handleChange(fakeEvent);
+                // that.props.handleChange(fakeEvent);
             });
         },
         componentDidUpdate: function () {
             this.updateSliderValues();
         },
         updateSliderValues: function () {
+            
             $(this.mySlider)
-                .bootstrapSlider("setAttribute", "min", this.props.min)
-                .bootstrapSlider("setAttribute", "max", this.props.max)
-                .bootstrapSlider("setAttribute", "step", this.props.step)
-                .bootstrapSlider("setValue", this.props.value);
+                .bootstrapSlider("setAttribute", "min", parseInt(this.props.min) )
+                .bootstrapSlider("setAttribute", "max", parseInt(this.props.max) )
+                .bootstrapSlider("setAttribute", "step", parseInt(this.props.step) )
+                .bootstrapSlider("setValue", parseInt(this.props.value, 10) );
 
             var sliderEnable = this.props.disabled === "disabled" ? false : true;
             var currentlyEnabled = $(this.mySlider).bootstrapSlider("isEnabled");
@@ -67,53 +50,6 @@ var SliderNative = function(
                 if (currentlyEnabled) {
                     $(this.mySlider).bootstrapSlider("disable");
                 }
-            }
-        }
-    });
-
-
-    var SliderNativeBootstrap = React.createClass({
-        mixins: [BrowserDetectMixin],
-        componentWillMount: function () {
-            // Although IE10+ displays the native range control,it:
-            //      a) looks crap
-            //      b) doesn't respond to its Input or Change events properly.
-            // So have augmented a feature test with some good, old-fashioned
-            // browser sniffing to always display the Bootstrap version on IE.
-            var ieVersion = this.detectIE();
-            if (ieVersion > 1 && ieVersion < 12) {
-                // IE up to version 11
-                this.supportsRange = false;
-            }
-            else {
-                // IE 12+ and all other browsers
-                // Test whether range input is accepted by creating such a field, then seeing what its
-                // type is set to.
-                var input = document.createElement('input');
-                input.setAttribute('type', 'range');
-                this.supportsRange = input.type !== "text" ? true : false;
-            }
-
-
-        },
-        render: function () {
-            var polyfill = typeof this.props.polyfill == "undefined" ? true : this.props.polyfill;
-            if (polyfill) {
-                if (this.supportsRange) {
-                    return (
-                        <SliderNative {...this.props} />
-                    );
-                }
-                else {
-                    return (
-                        <BootstrapSlider {...this.props} />
-                    );
-                }
-            }
-            else {
-                return (
-                    <BootstrapSlider {...this.props} />
-                );
             }
         }
     });
